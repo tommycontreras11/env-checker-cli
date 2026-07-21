290fa7c439b79adbb037b5141760386ea5d4395d
Tommy Grullon <tommycontreras34@gmail.com>
2026-07-21 09:32:07 -0400

    feat: complete env validations

--- env-checker.js
+++ env-checker.js
@@ -7,9 +7,42 @@ const args = process.argv.slice(3);
 const filePath = path.resolve(urlPath);
 const fileName = path.basename(filePath);
 
+const parseEnv = (entries) => {
+  const env = {};
+  const lines = entries.split("\n");
+
+  for (const line of lines) {
+    const [key, value] = line.split("=");
+
+    if (key) {
+      env[key.trim()] = value?.trim();
+    }
+  }
+  return env;
+};
+
+const validateEnv = (args, env) => {
+  const missingEnvs = [];
+
+  for (const arg of args) {
+    if (!(arg in env)) {
+      missingEnvs.push(arg);
+    }
+  }
+
+  if (missingEnvs.length > 0) {
+    process.stderr.write(
+      `error: missing environment variables: ${missingEnvs.join(", ")}\n`,
+    );
+    process.exit(1);
+  }
+};
+
 try {
   if (!args.length) {
-    process.stderr.write(`error: please provide at least one environment variable name\n`);
+    process.stderr.write(
+      `error: please provide at least one environment variable name\n`,
+    );
     process.exit(1);
   }
 
@@ -22,22 +55,12 @@ try {
     encoding: "utf8",
   });
 
-  const missingEnvs = []
+  const env = parseEnv(entries);
 
-  for(const arg of args) {
-    if(!entries.includes(arg)) {
-        missingEnvs.push(arg)
-    }
-  }
+  validateEnv(args, env);
 
-  if(missingEnvs.length > 0) {
-    process.stderr.write(`error: missing environment variables: ${missingEnvs.join(", ")}\n`);
-    process.exit(1);
-  }
-
-  console.log(`Set: ${filePath}`)
-  console.log("All required environment variables are set.")  
-
+  console.log(`Set: ${filePath}`);
+  console.log("All required environment variables are set.");
 } catch (error) {
   if (error.code === "ENOENT") {
     process.stderr.write(
